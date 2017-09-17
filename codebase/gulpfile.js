dd677097ffac45348a3612b95231c851c868568d
DarthHater <jeffryxtron@gmail.com>
2017-09-16 17:10:20 -0700

    Add in sass-lint

--- codebase/gulpfile.js
+++ codebase/gulpfile.js
@@ -1,10 +1,14 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var sass = require('gulp-sass');
-var connect = require('gulp-connect');
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	sass = require('gulp-sass'),
+	connect = require('gulp-connect'),
+	sassLint = require('gulp-sass-lint');
 
 gulp.task('sass', function () {
   gulp.src('./scss/*.scss')
+  	.pipe(sassLint())
+  	.pipe(sassLint.format())
+  	.pipe(sassLint.failOnError())
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest("assets/css"));
 });
