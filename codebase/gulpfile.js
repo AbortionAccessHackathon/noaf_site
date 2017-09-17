454661c489567cbecb74c2d5075d38b6b9e81689
DarthHater <jeffryxtron@gmail.com>
2017-09-17 08:36:56 -0700

    Sass-svg

--- codebase/gulpfile.js
+++ codebase/gulpfile.js
@@ -1,14 +1,14 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util'),
-	sass = require('gulp-sass'),
-	connect = require('gulp-connect'),
-	sassLint = require('gulp-sass-lint');
+  gutil = require('gulp-util'),
+  sass = require('gulp-sass'),
+  connect = require('gulp-connect'),
+  sassLint = require('gulp-sass-lint');
 
 gulp.task('sass', function () {
   gulp.src('./scss/**/*.scss')
-  	.pipe(sassLint())
-  	.pipe(sassLint.format())
-  	.pipe(sassLint.failOnError())
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest("assets/css"));
 });
